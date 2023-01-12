f01de5577f0f953f8f7c4eebf4e7f4a276057511
Andres Bolivar <andresmaster1@gmail.com>
2023-01-13 00:02:01 +0400

    Added option for stopping at time

--- cursor_move.py
+++ cursor_move.py
@@ -3,12 +3,13 @@
 import pyautogui
 import time
 
-now = "00:00"
-
-while now < "23:54":
+while True:
     now = time.strftime("%H:%M", time.localtime())
+    if now == "00:01":
+        break
     for i in range(-1, 2, 2):
         pyautogui.move(i, i)
         print(f"Time: {now} -- Position: {pyautogui.position()}")
         time.sleep(30)
+print(f"Stopped because now is: {now}")
     
