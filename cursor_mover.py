c2df8519fa4726aacdf42c0dac34920c9c22ee90
Andres Bolivar <andresmaster1@gmail.com>
2023-01-17 13:47:27 +0400

    Improving validations

--- cursor_mover.py
+++ cursor_mover.py
@@ -3,18 +3,20 @@
 import pyautogui
 import time
 import argparse
+import re
+import datetime
 
-def move_cursor_time(set_time):
-    print(f"This will stop at {set_time}")
+def move_cursor_time(custom_time):
+    print(f"This will stop at {custom_time.strftime('%H:%M')}")
     while True:
         for i in range(-1, 2, 2):
-            now = time.strftime('%H:%M:%S', time.localtime())
+            current_time = datetime.time(datetime.datetime.now().hour, datetime.datetime.now().minute)
+            if custom_time == current_time:
+                print(f"Stopped! Now is: {current_time.strftime('%h:%M')}")
+                exit(0)
             pyautogui.move(i, i)
-            print(f"Time: {now} -- Position: {pyautogui.position()}")
+            print(f"Time: {current_time.strftime('%h:%M')} -- Position: {pyautogui.position()}")
             time.sleep(60)
-        if now == set_time:
-            print(f"Stopped! Now is: {now}")
-            break
 
 def move_cursor():
     while True:
@@ -28,6 +30,17 @@ parser.add_argument("-t", metavar = 'HH:mm', dest = 'set_time', help = "Set time
 args = parser.parse_args()
 
 if args.set_time:
-    move_cursor_time(args.set_time)
+    try:
+        pattern = re.compile('ˆ[0-9]{1,2}\:[0-9]{2}$')
+        check = pattern.match(args.set_time)
+        hour_minute = args.set_time.split(":")
+        custom_time = datetime.time(int(hour_minute[0]), int(hour_minute[1]))
+        current_time = datetime.time(datetime.datetime.now().hour, datetime.datetime.now().minute)
+        if check and custom_time:
+            move_cursor_time(custom_time)
+    except ValueError:
+        print(f"You entered {args.set_time}, please use a time value under 23:59")
+    except IndexError:
+        print(f"You entered {args.set_time}, please use the correct format HH:mm in 24-hour, for example 13:30")
 else:
     move_cursor()
